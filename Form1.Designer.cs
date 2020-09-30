27237190915a8dc3d19bab480c80f8f3851599fb
ProgRub <ruben2000jgr@gmail.com>
2020-09-30 19:23:52 +0100

    Initial Commit, pratically done

---
+++ Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace ShoppingListHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl_addProduct = new System.Windows.Forms.Label();
+            this.checkBox_possibleProducts = new System.Windows.Forms.CheckedListBox();
+            this.entry_product = new System.Windows.Forms.TextBox();
+            this.btn_addProduct = new System.Windows.Forms.Button();
+            this.btn_makeExcelSheet = new System.Windows.Forms.Button();
+            this.dropdown_productCategories = new System.Windows.Forms.ComboBox();
+            this.lbl_dropdown = new System.Windows.Forms.Label();
+            this.btn_checkAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_addProduct
+            // 
+            this.lbl_addProduct.AutoSize = true;
+            this.lbl_addProduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_addProduct.ForeColor = System.Drawing.Color.White;
+            this.lbl_addProduct.Location = new System.Drawing.Point(249, 27);
+            this.lbl_addProduct.Name = "lbl_addProduct";
+            this.lbl_addProduct.Size = new System.Drawing.Size(180, 31);
+            this.lbl_addProduct.TabIndex = 1;
+            this.lbl_addProduct.Text = "Novo Produto";
+            // 
+            // checkBox_possibleProducts
+            // 
+            this.checkBox_possibleProducts.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(14)))), ((int)(((byte)(14)))));
+            this.checkBox_possibleProducts.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.checkBox_possibleProducts.CheckOnClick = true;
+            this.checkBox_possibleProducts.ColumnWidth = 250;
+            this.checkBox_possibleProducts.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox_possibleProducts.ForeColor = System.Drawing.Color.White;
+            this.checkBox_possibleProducts.FormattingEnabled = true;
+            this.checkBox_possibleProducts.Location = new System.Drawing.Point(63, 141);
+            this.checkBox_possibleProducts.MultiColumn = true;
+            this.checkBox_possibleProducts.Name = "checkBox_possibleProducts";
+            this.checkBox_possibleProducts.Size = new System.Drawing.Size(1112, 550);
+            this.checkBox_possibleProducts.Sorted = true;
+            this.checkBox_possibleProducts.TabIndex = 2;
+            // 
+            // entry_product
+            // 
+            this.entry_product.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.entry_product.Location = new System.Drawing.Point(445, 27);
+            this.entry_product.Name = "entry_product";
+            this.entry_product.Size = new System.Drawing.Size(541, 38);
+            this.entry_product.TabIndex = 3;
+            // 
+            // btn_addProduct
+            // 
+            this.btn_addProduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_addProduct.Location = new System.Drawing.Point(1011, 33);
+            this.btn_addProduct.Name = "btn_addProduct";
+            this.btn_addProduct.Size = new System.Drawing.Size(112, 31);
+            this.btn_addProduct.TabIndex = 4;
+            this.btn_addProduct.Text = "Adicionar";
+            this.btn_addProduct.UseVisualStyleBackColor = true;
+            this.btn_addProduct.Click += new System.EventHandler(this.btn_addProduct_Click);
+            // 
+            // btn_makeExcelSheet
+            // 
+            this.btn_makeExcelSheet.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_makeExcelSheet.Location = new System.Drawing.Point(1221, 707);
+            this.btn_makeExcelSheet.Name = "btn_makeExcelSheet";
+            this.btn_makeExcelSheet.Size = new System.Drawing.Size(197, 42);
+            this.btn_makeExcelSheet.TabIndex = 5;
+            this.btn_makeExcelSheet.Text = "Fazer Lista Excel";
+            this.btn_makeExcelSheet.UseVisualStyleBackColor = true;
+            this.btn_makeExcelSheet.Click += new System.EventHandler(this.btn_makeExcelSheet_Click);
+            // 
+            // dropdown_productCategories
+            // 
+            this.dropdown_productCategories.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dropdown_productCategories.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dropdown_productCategories.FormattingEnabled = true;
+            this.dropdown_productCategories.Items.AddRange(new object[] {
+            "Geral",
+            "Rúben",
+            "Mãe",
+            "Lígia",
+            "José"});
+            this.dropdown_productCategories.Location = new System.Drawing.Point(445, 83);
+            this.dropdown_productCategories.Name = "dropdown_productCategories";
+            this.dropdown_productCategories.Size = new System.Drawing.Size(541, 33);
+            this.dropdown_productCategories.TabIndex = 6;
+            this.dropdown_productCategories.SelectedIndex = 0;
+            // 
+            // lbl_dropdown
+            // 
+            this.lbl_dropdown.AutoSize = true;
+            this.lbl_dropdown.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_dropdown.ForeColor = System.Drawing.Color.White;
+            this.lbl_dropdown.Location = new System.Drawing.Point(263, 80);
+            this.lbl_dropdown.Name = "lbl_dropdown";
+            this.lbl_dropdown.Size = new System.Drawing.Size(166, 31);
+            this.lbl_dropdown.TabIndex = 7;
+            this.lbl_dropdown.Text = "Para Quem?";
+            // 
+            // btn_checkAll
+            // 
+            this.btn_checkAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_checkAll.Location = new System.Drawing.Point(1221, 141);
+            this.btn_checkAll.Name = "btn_checkAll";
+            this.btn_checkAll.Size = new System.Drawing.Size(161, 31);
+            this.btn_checkAll.TabIndex = 8;
+            this.btn_checkAll.Text = "Selecionar Todos";
+            this.btn_checkAll.UseVisualStyleBackColor = true;
+            this.btn_checkAll.Click += new System.EventHandler(this.btn_checkAll_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btn_addProduct;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(14)))), ((int)(((byte)(14)))));
+            this.ClientSize = new System.Drawing.Size(1430, 761);
+            this.Controls.Add(this.btn_checkAll);
+            this.Controls.Add(this.lbl_dropdown);
+            this.Controls.Add(this.dropdown_productCategories);
+            this.Controls.Add(this.btn_makeExcelSheet);
+            this.Controls.Add(this.btn_addProduct);
+            this.Controls.Add(this.entry_product);
+            this.Controls.Add(this.checkBox_possibleProducts);
+            this.Controls.Add(this.lbl_addProduct);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Construtor de Lista de Compras";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl_addProduct;
+        private System.Windows.Forms.CheckedListBox checkBox_possibleProducts;
+        private System.Windows.Forms.TextBox entry_product;
+        private System.Windows.Forms.Button btn_addProduct;
+        private System.Windows.Forms.Button btn_makeExcelSheet;
+        private System.Windows.Forms.ComboBox dropdown_productCategories;
+        private System.Windows.Forms.Label lbl_dropdown;
+        private System.Windows.Forms.Button btn_checkAll;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml;
+using System.Reflection;
+using ClosedXML.Excel;
+
+namespace ShoppingListHelper
+{
+    public partial class Form1 : Form
+    {
+        private string filename;
+        private string directory;
+        private string filePath;
+        private XDocument xmlProducts;
+        private bool checkAll;
+        public Form1()
+        {
+            InitializeComponent();
+            this.checkAll = false;
+            var heightField = typeof(CheckedListBox).GetField("scaledListItemBordersHeight", BindingFlags.NonPublic | BindingFlags.Instance);
+            var addedHeight = 5; // Some appropriate value, greater than the field's default of 2
+            heightField.SetValue(this.checkBox_possibleProducts, addedHeight); // Where "clb" is your CheckedListBox
+            this.filename = "Produtos.xml";
+            this.directory = Directory.GetCurrentDirectory();
+            this.filePath = Path.Combine(directory, filename);
+            this.xmlProducts = XDocument.Load(this.filePath);
+            try
+            {
+                //this.xmlProducts.Load(filePath);
+                var products = this.xmlProducts.Root.Elements("Product").ToList();
+                for (int index = 0; index < products.Count; index++)
+                {
+                    Console.WriteLine(products[index].Value);
+                    var productName = products[index].Value;
+                    if (Convert.ToInt32(products[index].Attribute("forWho").Value) == 1)
+                    {
+                        productName += " R";
+                    }
+                    else if (Convert.ToInt32(products[index].Attribute("forWho").Value) == 2)
+                    {
+                        productName += " Mãe";
+                    }
+                    else if (Convert.ToInt32(products[index].Attribute("forWho").Value) == 3)
+                    {
+                        productName += " L";
+                    }
+                    else if (Convert.ToInt32(products[index].Attribute("forWho").Value) == 4)
+                    {
+                        productName += " J";
+                    }
+                    int indexOfProduct = this.checkBox_possibleProducts.Items.Add(productName);
+                    if (Convert.ToBoolean(products[index].Attribute("selected").Value))
+                    {
+                        this.checkBox_possibleProducts.SetItemChecked(indexOfProduct, true);
+                    }
+                    else
+                    {
+                        this.checkBox_possibleProducts.SetItemChecked(indexOfProduct, false);
+                    }
+                }
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                File.Create(filePath).Close();
+                this.xmlProducts = new XDocument(new XElement("Root"));
+                this.xmlProducts.Save(this.filePath);
+                this.xmlProducts = XDocument.Load(this.filePath);
+            }
+        }
+
+        private void btn_addProduct_Click(object sender, EventArgs e)
+        {
+            string product = this.entry_product.Text.Trim();
+            if (product.Length != 0)
+            {
+                XElement productNode = new XElement("Product");
+                productNode.Value = product;
+                if (this.dropdown_productCategories.Text == "Rúben")
+                {
+                    product += " R";
+                    productNode.SetAttributeValue("forWho", "1");
+                }
+                else if (this.dropdown_productCategories.Text == "Mãe")
+                {
+                    product += " Mãe";
+                    productNode.SetAttributeValue("forWho", "2");
+                }
+                else if (this.dropdown_productCategories.Text == "Lígia")
+                {
+                    product += " L";
+                    productNode.SetAttributeValue("forWho", "3");
+                }
+                else if (this.dropdown_productCategories.Text == "José")
+                {
+                    product += " J";
+                    productNode.SetAttributeValue("forWho", "4");
+                }
+                else
+                {
+                    productNode.SetAttributeValue("forWho", "0");
+                }
+                productNode.SetAttributeValue("selected", true.ToString());
+                this.xmlProducts.Root.Add(productNode);
+                int index = this.checkBox_possibleProducts.Items.Add(product);
+                this.checkBox_possibleProducts.SetItemChecked(index, true);
+            }
+            this.entry_product.Text = "";
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            var productsList = xmlProducts.Root.Elements().OrderBy(element => element.Value).ThenByDescending(element=>element.Attribute("forWho").Value).ToList();
+            //Console.WriteLine(productsList.ToString());
+            xmlProducts.Root.Descendants().Remove();
+            //Console.WriteLine("HERE");
+            //foreach (XElement product in xmlProducts.Root.Descendants())
+            //    Console.WriteLine(product.Value);
+            for (int index = 0; index < productsList.Count; index++)
+            {
+                productsList[index].Attribute("selected").Value = this.checkBox_possibleProducts.GetItemChecked(index).ToString();
+            }
+            xmlProducts.Root.Add(productsList);
+            this.xmlProducts.Save(filePath);
+        }
+
+        private void btn_makeExcelSheet_Click(object sender, EventArgs e)
+        {
+            File.Delete(Path.Combine(this.directory, "Lista De Compras.xlsx"));
+            using (var workbook = new XLWorkbook())
+            {
+                Dictionary<string, int> cellsLetters_numberOfProducts = new Dictionary<string, int>();
+                List<string> productsNotForMom = new List<string>();
+                List<string> productsForMom = new List<string>();
+                var products = this.xmlProducts.Root.Elements().ToList();
+                for (int index = 0; index < products.Count; index++)
+                {
+                    if (this.checkBox_possibleProducts.GetItemChecked(index))
+                    {
+                        if (products[index].Attribute("forWho").Value == "2")
+                        {
+                            productsForMom.Add(products[index].Value);
+                        }
+                        else
+                        {
+                            if (products[index].Attribute("forWho").Value == "3")
+                            {
+                                productsNotForMom.Add(products[index].Value + " L");
+                            }
+                            else if (products[index].Attribute("forWho").Value == "4")
+                            {
+                                productsNotForMom.Add(products[index].Value + " J");
+                            }
+                            else if (products[index].Attribute("forWho").Value == "1")
+                            {
+                                productsNotForMom.Add(products[index].Value + " R");
+                            }
+                            else
+                            {
+                                productsNotForMom.Add(products[index].Value);
+                            }
+                        }
+                    }
+                }
+                var worksheet = workbook.Worksheets.Add("Lista De Compras");
+                int columnWidth = 20;
+                worksheet.Column("B").Width = 1;
+                worksheet.Column("D").Width = 1;
+                worksheet.Column("F").Width = 1;
+                worksheet.Style.Font.FontName = "Arial";
+                worksheet.Style.Font.FontSize = 12;
+                if (productsNotForMom.Count > 30)
+                {
+                    var minimumPerColumn = productsNotForMom.Count / 3;
+                    if (productsNotForMom.Count % 3 == 2)
+                    {
+                        cellsLetters_numberOfProducts.Add("A", minimumPerColumn + 1);
+                        cellsLetters_numberOfProducts.Add("C", minimumPerColumn + 1);
+                        cellsLetters_numberOfProducts.Add("E", minimumPerColumn);
+                    }
+                    else if (productsNotForMom.Count % 3 == 1)
+                    {
+                        cellsLetters_numberOfProducts.Add("A", minimumPerColumn + 1);
+                        cellsLetters_numberOfProducts.Add("C", minimumPerColumn);
+                        cellsLetters_numberOfProducts.Add("E", minimumPerColumn);
+                    }
+                    else
+                    {
+                        cellsLetters_numberOfProducts.Add("A", minimumPerColumn);
+                        cellsLetters_numberOfProducts.Add("C", minimumPerColumn);
+                        cellsLetters_numberOfProducts.Add("E", minimumPerColumn);
+                    }
+                    if (productsForMom.Count > 0)
+                    {
+                        worksheet.Cell("G1").Value = "MÃE";
+                        worksheet.Cell("G1").Style.Font.Bold = true;
+                        worksheet.Column("G").Width = columnWidth;
+                        for (int index = 0; index < productsForMom.Count; index++)
+                        {
+                            worksheet.Cell("G" + (index + 2).ToString()).Value = productsForMom[index];
+                        }
+                    }
+                }
+                else if (productsNotForMom.Count > 10)
+                {
+                    var minimumPerColumn = productsNotForMom.Count / 2;
+                    cellsLetters_numberOfProducts.Add("A", minimumPerColumn + productsNotForMom.Count % 2);
+                    cellsLetters_numberOfProducts.Add("C", minimumPerColumn);
+                    if (productsForMom.Count > 0)
+                    {
+                        worksheet.Cell("E1").Value = "MÃE";
+                        worksheet.Cell("E1").Style.Font.Bold = true;
+                        worksheet.Column("E").Width = columnWidth;
+                        for (int index = 0; index < productsForMom.Count; index++)
+                        {
+                            worksheet.Cell("E" + (index + 2).ToString()).Value = productsForMom[index];
+                        }
+                    }
+                }
+                else
+                {
+                    cellsLetters_numberOfProducts.Add("A", productsNotForMom.Count);
+                    if (productsForMom.Count > 0)
+                    {
+                        worksheet.Cell("C1").Value = "MÃE";
+                        worksheet.Cell("C1").Style.Font.Bold = true;
+                        worksheet.Column("C").Width = columnWidth;
+                        for (int index = 0; index < productsForMom.Count; index++)
+                        {
+                            worksheet.Cell("C" + (index + 2).ToString()).Value = productsForMom[index];
+                        }
+                    }
+                }
+                var listIndex = 0;
+                foreach (var cellLetter in cellsLetters_numberOfProducts.Keys)
+                {
+                    worksheet.Column(cellLetter).Width = columnWidth;
+                    for (int columnIndex = 0; columnIndex < cellsLetters_numberOfProducts[cellLetter]; columnIndex++)
+                    {
+                        worksheet.Cell(cellLetter + (columnIndex + 1).ToString()).Value = productsNotForMom[listIndex];
+                        listIndex++;
+                    }
+                }
+                var excelFilePath = Path.Combine(this.directory, "Lista De Compras.xlsx");
+                workbook.SaveAs(excelFilePath);
+            }
+        }
+
+        private void btn_checkAll_Click(object sender, EventArgs e)
+        {
+            this.checkAll = !this.checkAll;
+            for (int index = 0; index < this.checkBox_possibleProducts.Items.Count; index++)
+            {
+                this.checkBox_possibleProducts.SetItemChecked(index, this.checkAll);
+            }
+        }
+    }
+}
